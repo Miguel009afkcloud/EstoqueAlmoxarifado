f4db10584445eb98948300b5c1350a3ababa7787
Miguel009afkcloud <miguellucena156@gmail.com>
2026-09-18 17:30:40 -0400

    atualizando

--- obj/Release/net10.0/EstoqueAlmoxarifado.API.AssemblyInfo.cs
+++ obj/Release/net10.0/EstoqueAlmoxarifado.API.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("EstoqueAlmoxarifado.API")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ded45ed6d50225a413baed68ae404625a10e4b81")]
 [assembly: System.Reflection.AssemblyProductAttribute("EstoqueAlmoxarifado.API")]
